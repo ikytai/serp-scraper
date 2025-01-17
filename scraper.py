80989440a140e1b9510f7c7e8058478eb8794826
ikytai <59370833+ikytai@users.noreply.github.com>
2025-01-17 12:36:23 +0000

    Update scraper.py
    adding employee stock options

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ base_params = {
 }
 
 # Define the list of queries
-queries = ['espp', 'long term incentive plan', 'equity compensation', 'stock plan administration', 'stock plan administration software', 'equity management software', 'equity management platform']
+queries = ['espp', 'long term incentive plan', 'equity compensation', 'stock plan administration', 'stock plan administration software', 'equity management software', 'equity management platform', 'employee stock options']
 
 # Initialize an empty list to hold all results
 all_results = []
